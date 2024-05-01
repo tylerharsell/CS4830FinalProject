9373cf0bd5643eebe95b47489948ee2baa843b67
tylerharsell <tharsell20@gmail.com>
2024-05-01 15:30:42 -0500

    mongodb connection

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
-const http = require("http");
-const debug = require("debug")("node-angular");
 const app = require("./backend/app");
+const debug = require("debug")("node-angular");
+const http = require("http");
 
-const normalizePort = val => {
-  let port = parseInt(val, 10);
-  if (isNaN(port)) return val;
-  if (port >= 0) return port;
+const normalizePort = (val) => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
   return false;
 };
 
-const onError = error => {
-  if (error.syscall !== "listen") throw error;
+const onError = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
   switch (error.code) {
     case "EACCES":
       console.error(bind + " requires elevated privileges");
       process.exit(1);
+      break;
     case "EADDRINUSE":
       console.error(bind + " is already in use");
       process.exit(1);
+      break;
     default:
       throw error;
   }
@@ -26,7 +39,7 @@ const onError = error => {
 
 const onListening = () => {
   const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
   debug("Listening on " + bind);
 };
 
